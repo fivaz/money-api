93c1f1ed273e3435ff4d13a720e8fcc752c08ea3
sfivaz <s.a.fivaz@gmail.com>
2019-07-19 20:58:44 +0200

    sequelize tables created with foreign keys

--- src/app/infra/database.js
+++ src/app/infra/database.js
@@ -1,9 +1,76 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('money', 'root', 'root', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: 3307,
 });
 
-sequelize.authenticate()
-    .then(() => console.log("ok"))
-    .catch(erro => console.log("not ok" + erro));
+const Model = Sequelize.Model;
+
+class Account extends Model {
+}
+
+Account.init(
+    {name: {type: Sequelize.STRING}},
+    {sequelize, modelName: 'accounts'}
+);
+
+class Category extends Model {
+}
+
+Category.init(
+    {name: {type: Sequelize.STRING}},
+    {sequelize, modelName: 'categories'});
+
+class Transaction extends Model {
+}
+
+Transaction.init({
+        description: {type: Sequelize.STRING},
+        value: {type: Sequelize.DOUBLE},
+        date: {type: Sequelize.DATE},
+        type: {type: Sequelize.STRING},
+        category: {
+            type: Sequelize.INTEGER,
+            references: {
+                model: Category,
+                key: 'id'
+            }
+        },
+        sourceAccount: {
+            type: Sequelize.INTEGER,
+            references: {
+                model: Account,
+                key: 'id'
+            }
+        },
+        destinationAccount: {
+            type: Sequelize.INTEGER,
+            references: {
+                model: Account,
+                key: 'id'
+            }
+        }
+    },
+    {sequelize, modelName: 'transactions'}
+);
+
+
+/*
+Accounts.create({name: "Bank"});
+Categories.create({name: "Supermarket"});
+Transactions.create({
+    description: "Test1",
+    value: 20.05,
+    date: new Date(),
+    type: "Spending",
+    sourceAccount: 1,
+    destinationAccount: null
+});
+*/
+
+// Account.sync({force: true});
+// Category.sync({force: true});
+Transaction.sync({force: true});
+
+module.exports = sequelize;
--- src/app/infra/routes.js
+++ src/app/infra/routes.js
@@ -1,16 +1,10 @@
+const sequelize = require('./database');
+
 module.exports = (app) => {
 
     app.get("/accounts", (req, res) => {
-        const Sequelize = require('sequelize');
-        const sequelize = new Sequelize('money', 'root', 'root', {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3307
-        });
-
         sequelize.authenticate()
             .then(() => console.log("ok"))
             .catch(erro => console.log("not ok" + erro));
-
     });
 };
