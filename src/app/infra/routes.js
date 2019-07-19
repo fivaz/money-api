ec270522be2bd38351eda05a7c61d3e43cba3470
sfivaz <s.a.fivaz@gmail.com>
2019-07-19 20:04:25 +0200

    connexion stablished to sequelize/mysql

--- src/app/infra/routes.js
+++ src/app/infra/routes.js
@@ -1,6 +1,16 @@
 module.exports = (app) => {
 
     app.get("/accounts", (req, res) => {
-        console.log("Ola");
+        const Sequelize = require('sequelize');
+        const sequelize = new Sequelize('money', 'root', 'root', {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3307
+        });
+
+        sequelize.authenticate()
+            .then(() => console.log("ok"))
+            .catch(erro => console.log("not ok" + erro));
+
     });
 };
