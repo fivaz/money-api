8848bda400f8c71cbaf17918ce07886164a14256
sfivaz <s.a.fivaz@gmail.com>
2019-07-20 10:16:58 +0200

    finding transactions populated

--- src/app/infra/proxies/TransactionProxy.js
+++ src/app/infra/proxies/TransactionProxy.js
@@ -1,11 +1,53 @@
 const Proxy = require('./Proxy');
 const Transaction = require('../models/Transaction');
+const Category = require('../models/Category');
+const Account = require('../models/Account');
 
 class TransactionProxy extends Proxy {
 
     constructor() {
         super();
         this.model = Transaction;
+    }
+
+    findAll(req, res) {
+        this.model
+            .findAll({
+                include: [
+                    {model: Category},
+                    {
+                        model: Account,
+                        as: 'sourceAccount'
+                    },
+                    {
+                        model: Account,
+                        as: 'destinationAccount'
+                    }
+                ]
+            })
+            .then(rows => res.json(rows))
+            .catch(errors =>
+                res.json({
+                    status: 412,
+                    message: errors
+                }));
+    }
+
+    find(req, res) {
+        this.model
+            .findByPk(req.params.id)
+            .then(transaction => {
+                if (transaction) {
+                    transaction.getSourceAccount()
+                        .then(category => res.json(category));
+                } else
+                    res.sendStatus(204);
+            })
+            .catch(errors =>
+                res.json({
+                    status: 412,
+                    message: errors
+                }));
     }
 
     /*
