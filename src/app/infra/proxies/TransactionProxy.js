2b6b553fb0469a6c65d0f0a5efeac74167bbacc6
sfivaz <s.a.fivaz@gmail.com>
2019-07-21 15:17:00 +0200

    fea - implement method to get all transactions related to an account

--- src/app/infra/proxies/TransactionProxy.js
+++ src/app/infra/proxies/TransactionProxy.js
@@ -11,7 +11,7 @@ class TransactionProxy extends Proxy {
         this.model = Transaction;
     }
 
-    findAll(req, res) {
+    findPopulated(req, res) {
         this.model
             .findAll({
                 include: [
@@ -34,57 +34,25 @@ class TransactionProxy extends Proxy {
                 }));
     }
 
-    find(req, res) {
+    findFrom(req, res) {
+        const Op = db.Sequelize.Op;
         this.model
-            .findByPk(req.params.id)
-            .then(transaction => {
-                if (transaction) {
-                    transaction.getSourceAccount()
-                        .then(category => res.json(category));
-                } else
-                    res.sendStatus(204);
+            .findAll({
+                where: {
+                    [Op.or]: [
+                        {sourceAccountId: req.params.accountId},
+                        {destinationAccountId: req.params.accountId},
+                    ]
+                }
             })
+            .then(transactions =>
+                res.json(transactions))
             .catch(errors =>
                 res.json({
                     status: 412,
                     message: errors
                 }));
     }
-
-    /*
-    create(req, res) {
-        Account.findOne(req.body.sourceAccount,
-            (errors, account) => {
-                if (errors)
-                    console.log(errors);
-                else if (!account)
-                    res.sendStatus(412);
-                else {
-                    this.model.create(req.body,
-                        (errors, transaction) => {
-                            if (errors)
-                                res.json(pretty(errors));
-                            else
-                                res.json(transaction);
-                        });
-                }
-            });
-    }
-
-    find(req, res) {
-        this.model.find(
-            {
-                sourceAccount:
-                    {_id: req.params.accountId}
-            },
-            (errors, rows) => {
-                if (errors)
-                    console.log(errors);
-                else
-                    res.json(rows);
-            });
-    }
-     */
 }
 
 module.exports = TransactionProxy;
