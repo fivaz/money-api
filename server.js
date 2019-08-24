964f47d797e79c08b93fa9ff36a8d91363de67f1
sfivaz <s.a.fivaz@gmail.com>
2019-08-24 02:50:53 +0200

    ref - using environment variable for PORT / ref - Procfile file create

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/config/custom-express');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Server running in the port ${port}`));
