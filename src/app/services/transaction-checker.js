27127337e99273a83abee19ff928e30406245cc8
sfivaz <s.a.fivaz@gmail.com>
2019-09-03 00:31:16 +0200

    fea - if there is only one remaining month to a monthly transaction, so use cloneTransactionOnce

--- src/app/services/transaction-checker.js
+++ src/app/services/transaction-checker.js
@@ -23,43 +23,70 @@ class TransactionChecker {
     async execCheck() {
         console.log("check executed");
         const transactions = await this.ORM.findMonthly();
-
         const createdTransactions = transactions.map(transaction => {
-            const transactionRaw = transaction.get({plain: true});
-            const dates = TransactionChecker.getMonthlyDates(transactionRaw.date);
-            return this.checkThenDuplicateTransaction(transactionRaw, dates);
+            //TODO check it later
+            const dates = TransactionChecker.getRemainingMonths(transaction.date);
+            console.log(dates);
+            if (dates.length === 1)
+                return this.cloneTransactionOnce(transaction, dates[0]);
+            else {
+                console.log("ELSE");
+                return Promise.resolve(1);
+            }
+            // else if (dates.length > 2)
+            //     return this.checkThenCloneTransaction(transactionRaw, dates);
         });
 
         return Promise.all(createdTransactions);
     }
 
-    checkThenDuplicateTransaction(transaction, dates) {
+    cloneTransactionOnce(transaction, date) {
+        const firstStep = this.transactionRemoveMonthly(transaction);
+        const secondStep = this.createTransaction(transaction, date);
+        return Promise.all([firstStep, secondStep]);
+    }
+
+    transactionRemoveMonthly(transaction) {
+        const transactionRaw = transaction.get({plain: true});
+        transactionRaw.isMonthly = 0;
+        return this.ORM.update(transactionRaw, transactionRaw.id);
+    }
+
+    createTransaction(transaction, date) {
+        const transactionRaw = transaction.get({plain: true});
+        delete transactionRaw.id;
+        transactionRaw.isMonthly = 1;
+        transactionRaw.date = date;
+        return this.ORM.create(transactionRaw);
+    }
+
+    checkThenCloneTransaction(transaction, dates) {
         const createdTransactions = dates.map(async date => {
             const exist = await this.ORM.existIn(transaction, date);
             if (!exist)
-                return this.duplicateTransaction(transaction, date);
+                return this.cloneTransaction(transaction, date);
             else
                 return Promise.resolve();
         });
         return Promise.all(createdTransactions);
     }
 
-    duplicateTransaction(transaction, date) {
+    cloneTransaction(transaction, date) {
         delete transaction.id;
         transaction.isMonthly = 0;
         transaction.date = date;
         return this.ORM.model.create(transaction);
     }
 
-    static getMonthlyDates(date) {
+    static getRemainingMonths(date) {
         const transactionDate = moment(date);
         const today = moment();
         const nbOfMonths = today.diff(transactionDate, 'months');
         const dates = [];
         let i = 0;
-        while (i <= nbOfMonths) {
-            dates.push(transactionDate.clone().toDate());
+        while (i < nbOfMonths) {
             transactionDate.add(1, 'months');
+            dates.push(transactionDate.clone().toDate());
             i++;
         }
         return dates;
