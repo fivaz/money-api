1f178da6c1fef51c88c4fa71f9ad83fe50a85148
sfivaz <s.a.fivaz@gmail.com>
2019-09-03 20:32:36 +0200

    ref - TransactionRouterHelper.create and update removed / using those from Super class RouterHelper

--- src/app/helpers/TransactionRouterHelper.js
+++ src/app/helpers/TransactionRouterHelper.js
@@ -1,6 +1,5 @@
 const TransactionProxy = require('../infra/proxies/TransactionProxy');
 const RouterHelper = require('./RouterHelper');
-const transactionChecker = require('../services/transaction-checker');
 
 class TransactionRouterHelper extends RouterHelper {
 
@@ -18,23 +17,6 @@ class TransactionRouterHelper extends RouterHelper {
     selectFrom(req, res) {
         this.model.findFrom(req.params.accountId)
             .then(transactions => res.json(transactions))
-            .catch(errors => RouterHelper.sendResponse(res, 412, errors));
-    }
-
-    create(req, res) {
-        this.model.create(req.body)
-            .then(transactions => res.json(transactions))
-            .catch(errors => RouterHelper.sendResponse(res, 412, errors));
-    }
-
-    update(req, res) {
-        this.model
-            .update(req.body, req.params.id)
-            .then(row => {
-                res.json(row);
-                // noinspection JSIgnoredPromiseFromCall
-                transactionChecker.checkNow();
-            })
             .catch(errors => RouterHelper.sendResponse(res, 412, errors));
     }
 }
