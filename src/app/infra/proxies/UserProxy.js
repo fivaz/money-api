f33da4ed3c035a4a98a3a34218c0f6d38499db0c
sfivaz <s.a.fivaz@gmail.com>
2019-09-07 15:39:34 +0200

    ref - UserProxy.login using encrypted password

--- src/app/infra/proxies/UserProxy.js
+++ src/app/infra/proxies/UserProxy.js
@@ -1,5 +1,6 @@
 const Proxy = require('./Proxy');
 const db = require('../database');
+const bcrypt = require('bcrypt');
 const User = db.User;
 
 class UserProxy extends Proxy {
@@ -9,9 +10,17 @@ class UserProxy extends Proxy {
         this.model = User;
     }
 
-    login(email, password) {
+    async login(email, password) {
+        const user = await this.findByEmail(email);
+        if (user)
+            return bcrypt.compare(password, user.password);
+        else
+            return null;
+    }
+
+    findByEmail(email) {
         return this.model.findOne({
-            where: {email, password}
+            where: {email}
         });
     }
 }
