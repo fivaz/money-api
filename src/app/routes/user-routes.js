0495b0789ed66685aa125d6bf741b68d996d56a7
sfivaz <s.a.fivaz@gmail.com>
2019-10-03 15:40:35 +0200

    Revert "cre - route for db info"
    This reverts commit 87a5b1f526d23939cfb04e47b4a4bc7ea3e4133d.

--- src/app/routes/user-routes.js
+++ src/app/routes/user-routes.js
@@ -13,6 +13,4 @@ module.exports = (app) => {
     app.post("/login", (req, res) => helper.login(req, res));
 
     app.delete("/users/:id", verifyToken, (req, res) => helper.delete(req, res));
-
-    app.get("/info", verifyToken, (req, res) => helper.info(req, res))
 };
